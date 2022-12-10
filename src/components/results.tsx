de7e1d3513c28e6cf70cfdb9df6198d43f179fe2
ehockedy <edhockedy@hotmail.com>
2022-12-10 11:45:37 +0000

    Made the result lines expandable so can add further journey info

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,5 +1,5 @@
-import classNames from "classnames";
-import React from "react";
+import classnames from "classnames";
+import React, { useState } from "react";
 import { PlaceRankSummaries } from "../__generated__/types"
 import { Place } from "./place";
 import styles from "/src/css/results.css"
@@ -9,6 +9,33 @@ const formatTravelTime = (seconds: number) => {
     var minutes = Math.floor((seconds - (hours * 3600)) / 60);
     return `${hours}h ${minutes}m`
 }
+
+type ResultsLineProps = {
+    placeName: string,
+    travelTimePerMonth: number,
+}
+const ResultsLine = (props: ResultsLineProps) => {
+    const [expand, setExpand] = useState<boolean>(false)
+    return <div className={styles.resultLineContainer}>
+        <div className={styles.resultLine} >
+            <div className={styles.name}>
+                {props.placeName}
+            </div>
+            <div className={styles.travelTimePerMonth}>
+                {formatTravelTime(props.travelTimePerMonth)}
+            </div>
+            <button className={styles.expand} onClick={() => setExpand(!expand)}>{">"}</button>
+        </div>
+        <div className={classnames(styles.resultLine, styles.moreInfo, {[styles.moreInfoExpanded]: expand})}>
+            <div className={classnames(styles.moreInfoContent)}>
+                <div>efhiwefuhewf</div>
+                <div>efhiwefuhewf</div>
+                <div>efhiwefuhewf</div>
+            </div>
+        </div>
+    </div>
+}
+
 
 type ResultsProps = {
     results: PlaceRankSummaries;
@@ -31,15 +58,8 @@ const Results = (props: ResultsProps) => {
             <div className={styles.resultLines}>
                 {props.results.filter((result) => result.success).map((result) => {
                     const potentialHome = props.potentialHomes.find((ph) => ph.id === result.name)
-                    const name = potentialHome?.name;
-                    return <div className={styles.resultLine} key={result.name}>
-                        <div className={styles.name}>
-                            {name ? name : result.name}
-                        </div>
-                        <div className={styles.travelTimePerMonth}>
-                            {formatTravelTime(result.totalTravelTimePerMonth)}
-                        </div>
-                    </div>
+                    const name = potentialHome?.name || result.name;
+                    return <ResultsLine placeName={name} travelTimePerMonth={result.totalTravelTimePerMonth} key={name} />
                 })}
             </div>
         </div>
